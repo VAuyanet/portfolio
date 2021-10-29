711fc064f9a6b118198c1c23f9a69a3a5926e277
VAuyanet <victor.auyanet@gmail.com>
2021-10-29 19:15:30 +0200

    Basic form implementation

--- js/pages/contactForm.js
+++ js/pages/contactForm.js
@@ -1,39 +1,83 @@
+let form;
+
 document.onreadystatechange = function () {
   if (document.readyState === "interactive") {
+    form = document.getElementById("contact-form");
     initFormEvents();
   }
 };
 
 function initFormEvents() {
-  let form = document.getElementById("contact-form");
-  form.addEventListener("submit", async function (event) {
-    await handleSubmit(event);
-  });
+  if (form) {
+    form.addEventListener("submit", handleSubmit);
+  }
+}
+
+function areFormRequiredFieldsValid(formData) {
+  const firstName = isFormRequiredFieldValid(formData, "first-name");
+  const lastName = isFormRequiredFieldValid(formData, "last-name");
+  const email = isFormRequiredFieldValid(formData, "email", "email");
+  const message = isFormRequiredFieldValid(formData, "message");
+
+  return firstName && lastName && email && message;
+}
+
+function isFormRequiredFieldValid(formData, name, type = "text") {
+  const fieldValue = formData.get(name);
+  switch (type) {
+    case "email":
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(fieldValue);
+    case "text":
+    default:
+      return fieldValue !== "";
+  }
+
 }
 
 async function handleSubmit(event) {
   event.preventDefault();
-  let status = document.getElementById("contact-form-status");
-  let data = new FormData(event.target);
+  const formData = new FormData(event.target);
 
-  let reCaptchaResponse = window.grecaptcha.getResponse();
-  console.log(data);
-  if (reCaptchaResponse.length) {
-    let reCaptchaValidationResponse = validateCaptcha(reCaptchaResponse);
-    if (reCaptchaValidationResponse.success) {
-      //TODO: SEND EMAIL
-    }
+  if (areFormRequiredFieldsValid(formData)) {
+    fetch(event.target.action,
+      {
+        method: form.method,
+        body: formData,
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
+    ).then((response) => {
+
+      if (response.ok) {
+        const formWrapper = document.getElementById("contact-form-wrapper");
+        if (formWrapper) {
+          formWrapper.classList.add("form-success");
+        }
+        form.reset();
+      } else {
+        console.error("Contact Form KO");
+        setContactFormError();
+      }
+    }).catch((error) => {
+      console.error(error);
+      setContactFormError();
+    });
   } else {
-    status.innerHTML = "Oops! There was a problem. Please check that the reCaptcha is done"
+    setContactFormError("All required fields have to be filled out");
   }
 }
 
 
-async function validateCaptcha(token) {
-  const url = "https://www.google.com/recaptcha/api/siteverify";
-  const body = {
-    "secret": "6LdMv48cAAAAAL5ILctrmkmN5lJJHN032VJgBNbM",
-    "response": token
-  };
-  return await customPost(url, body);
+function setContactFormError(error = "") {
+
+  const errorStatus = document.getElementById("contact-form-error");
+  if (error !== "") {
+    errorStatus.innerHTML = error;
+  } else {
+    errorStatus.innerHTML = "<b>Oops!</b> There was a problem submitting your form. Remember to fill the captcha"
+  }
+  errorStatus.classList.add("active");
+
 }
